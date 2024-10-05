84b9b19faa7c267312d8c3f4072cfaf5ad0f82d1
Karen Payne <paynekaren@comcast.net>
2024-10-05 11:38:03 -0700

    list changed stub

--- DataGridViewCheckBoxApp1/Form2.cs
+++ DataGridViewCheckBoxApp1/Form2.cs
@@ -1,4 +1,5 @@
-﻿using DataGridViewCheckBoxApp1.Classes;
+﻿using System.ComponentModel;
+using DataGridViewCheckBoxApp1.Classes;
 using DataGridViewCheckBoxApp1.Models;
 using System.Text.Json;
 
@@ -27,6 +28,36 @@ public partial class Form2 : Form
 
         dataGridView1.Columns["UnitPrice"]!.DefaultCellStyle.Format = "C2";
         dataGridView1.Columns["UnitsInStock"]!.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+
+        _bindingList.ListChanged += _bindingList_ListChanged;
+    }
+
+    /// <summary>
+    /// Handles the <see cref="SortableBindingList{T}.ListChanged"/> event of the <see cref="SortableBindingList{T}"/>.
+    /// This method processes changes to the list, such as item changes, additions, and deletions.
+    /// </summary>
+    private void _bindingList_ListChanged(object? sender, System.ComponentModel.ListChangedEventArgs e)
+    {
+        if (e.ListChangedType == ListChangedType.ItemChanged)
+        {
+            if (e.PropertyDescriptor?.DisplayName == "Process")
+            {
+                // check/uncheck - handle it
+            }
+            else
+            {
+                // another property value changed
+            }
+
+        }
+        else if (e.ListChangedType == ListChangedType.ItemAdded)
+        {
+            // handle add new row e.g. FluidValidation 
+        }else if (e.ListChangedType == ListChangedType.ItemDeleted)
+        {
+            // If handling this make sure to also handle the DataGridView event
+        }
+
     }
 
     /// <summary>
